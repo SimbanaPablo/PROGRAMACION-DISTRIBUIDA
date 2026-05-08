3e2a7b7d2a390f1a151b9c9b6f106e01a2defb44
SimbanaPablo <pablo.simbana01@gmail.com>
2026-05-08 07:40:34 -0500

    conexion-bd

---
+++ app-authors/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("java")
+    id("io.quarkus") version "3.35.2"
+    id("io.freefair.lombok") version "9.2.0"
+
+}
+
+group = "org.example"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+val quarkusVersion = "3.35.2"
+dependencies {
+    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:${quarkusVersion}"))
+
+    // CDI
+    implementation("io.quarkus:quarkus-arc")
+
+    // REST
+    implementation("io.quarkus:quarkus-rest")
+    implementation("io.quarkus:quarkus-rest-jsonb")
+
+    implementation("io.quarkus:quarkus-hibernate-orm")
+    implementation("io.quarkus:quarkus-hibernate-orm-panache")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
+
+    implementation("io.quarkus:quarkus-flyway")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:12.5.0")
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
+}
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
